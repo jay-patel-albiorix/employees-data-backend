3095a74d209f296ed2228e68d3738478c5f0872f
jay-patel-albiorix <jaypatel.albiorix@gmail.com>
2021-07-22 13:29:13 +0530

    projecting after sort

--- controllers/appControllers.js
+++ controllers/appControllers.js
@@ -13,12 +13,12 @@ module.exports.getEmployeeList = async (req, res) => {
                 $match: JSON.parse(_get(req, "query.filter", "{}") ) // manual filters from frontend
             },
             {
-                $project: JSON.parse(_get(req, "query.keys", '{"_id": 1}'))
-            },
-            {
                 $sort: {
                     "createdAt": -1
                 }
+            },
+            {
+                $project: JSON.parse(_get(req, "query.keys", '{"_id": 1}'))
             },
             {
                 $facet: {
