a4c000c3469059bf8611b92d02b833ca91f289b0
jay-patel-albiorix <jaypatel.albiorix@gmail.com>
2021-08-23 15:41:41 +0530

    instance of datasource

--- graphql/dataSources/employees.js
+++ graphql/dataSources/employees.js
@@ -5,183 +5,189 @@ const _trim = require("lodash/trim")
 
 const Employee = require("../../models/Employee")
 
-module.exports.getEmployeeList = async (
-    keys = '{"_id": 1}',
-    search = "",
-    skip = 0,
-    limit = 10,
-) => {
-    try {
+class EmployeeDataSource {
 
-        const split = _split(_trim(search), / +/)
-        const filter = search ? {
-            $or: [
-                ..._map(
-                    split,
-                    splitWord => (
-                        {
-                            $expr: {
-                                $regexMatch: {
-                                    input: "$personal_details.first_name",
-                                    regex: splitWord,
-                                    options: "i"
+    getEmployeeList = async (
+        keys = '{"_id": 1}',
+        search = "",
+        skip = 0,
+        limit = 10,
+    ) => {
+        try {
+            console.log("getEmployeeList", "keys", keys, "search", search, "skip", skip, "limit", limit)
+            const split = _split(_trim(search), / +/)
+            const filter = search ? {
+                $or: [
+                    ..._map(
+                        split,
+                        splitWord => (
+                            {
+                                $expr: {
+                                    $regexMatch: {
+                                        input: "$personal_details.first_name",
+                                        regex: splitWord,
+                                        options: "i"
+                                    }
                                 }
+    
                             }
-
-                        }
-                    )
-                ),
-                ..._map(
-                    split,
-                    splitWord => (
-                        {
-                            $expr: {
-                                $regexMatch: {
-                                    input: "$personal_details.last_name",
-                                    regex: splitWord,
-                                    options: "i"
+                        )
+                    ),
+                    ..._map(
+                        split,
+                        splitWord => (
+                            {
+                                $expr: {
+                                    $regexMatch: {
+                                        input: "$personal_details.last_name",
+                                        regex: splitWord,
+                                        options: "i"
+                                    }
                                 }
+    
                             }
-
-                        }
-                    )
-                ),
-                ..._map(
-                    split,
-                    splitWord => (
-                        {
-                            $expr: {
-                                $regexMatch: {
-                                    input: "$current_work.designation",
-                                    regex: splitWord,
-                                    options: "i"
+                        )
+                    ),
+                    ..._map(
+                        split,
+                        splitWord => (
+                            {
+                                $expr: {
+                                    $regexMatch: {
+                                        input: "$current_work.designation",
+                                        regex: splitWord,
+                                        options: "i"
+                                    }
                                 }
+    
                             }
-
-                        }
-                    )
-                ),
-                ..._map(
-                    split,
-                    splitWord => (
-                        {
-                            $expr: {
-                                $regexMatch: {
-                                    input: "$current_work.department",
-                                    regex: splitWord,
-                                    options: "i"
+                        )
+                    ),
+                    ..._map(
+                        split,
+                        splitWord => (
+                            {
+                                $expr: {
+                                    $regexMatch: {
+                                        input: "$current_work.department",
+                                        regex: splitWord,
+                                        options: "i"
+                                    }
                                 }
+    
                             }
-
-                        }
+                        )
                     )
-                )
-            ]
-        } : {}
-
-        console.log("\nlist filter", filter)
-
-        const [ { data, meta: [ meta ] } ] = await Employee.aggregate([
-            {
-                $match: filter   // manual filters from frontend
-            },
-            {
-                $sort: {
-                    "createdAt": -1
+                ]
+            } : {}
+    
+            // console.log("\nlist filter", filter)
+    
+            const [ { data, meta: [ meta ] } ] = await Employee.aggregate([
+                {
+                    $match: filter   // manual filters from frontend
+                },
+                {
+                    $sort: {
+                        "createdAt": -1
+                    }
+                },
+                {
+                    $project: JSON.parse(keys)   // fetching db docs with required keys
+                },
+                {
+                    $facet: {
+                        data: [
+                            {
+                                $skip: _toInteger(skip)
+                            },
+                            {
+                                $limit: _toInteger(limit)
+                            },
+                        ],
+                        meta: [{
+                            $count: "count"
+                        }]
+                    }
                 }
-            },
-            {
-                $project: JSON.parse(keys)   // fetching db docs with required keys
-            },
-            {
-                $facet: {
-                    data: [
-                        {
-                            $skip: _toInteger(skip)
-                        },
-                        {
-                            $limit: _toInteger(limit)
-                        },
-                    ],
-                    meta: [{
-                        $count: "count"
-                    }]
-                }
+            ])
+    
+           return {
+                data,
+                meta: meta || {count: 0},
             }
-        ])
-
-       return {
-            data,
-            meta: meta || {count: 0},
+        } catch(err) {
+            console.log(err)
+            throw new Error("getting list failed")
         }
-    } catch(err) {
-        console.log(err)
-        throw new Error("getting list failed")
+    
     }
-
-}
-
-
-module.exports.getEmployee = async (_id) => {
-    try {
-        return await Employee.findById(_id)
-        
-    } catch(err) {
-        console.log(err)
-        throw new Error(err.message || "getting doc failed")
+    
+    
+    getEmployee = async (_id) => {
+        try {
+            return await Employee.findById(_id)
+            
+        } catch(err) {
+            console.log(err)
+            throw new Error(err.message || "getting doc failed")
+        }
+    
     }
-
-}
-
-
-module.exports.postEmployee = async (data) => {
-    try {
-        const newEmployee = new Employee(data)
-
-        const savedEmployee = await newEmployee.save()  
-
-        return savedEmployee      
-    } catch(err) {
-        console.log(err)
-        throw new Error(err.message || "Posting doc failed")
+    
+    
+    postEmployee = async (data) => {
+        try {
+            const newEmployee = new Employee(data)
+    
+            const savedEmployee = await newEmployee.save()  
+    
+            return savedEmployee      
+        } catch(err) {
+            console.log(err)
+            throw new Error(err.message || "Posting doc failed")
+        }
+    
     }
-
-}
-
-
-module.exports.putEmployee = async (_id, data) => {
-    try {
-        console.log("dataSource putEmployee", _id, data)
-        const updatedEmployee = await Employee.findOneAndReplace(
-            {_id},
-            data,
-            {
-                new: true,
-                upsert: false,
-            }
-        )
-
-        if(!updatedEmployee) throw new Error("No document found")
-
-        return updatedEmployee
-
-    } catch(err) {
-        console.log(err)
-        throw new Error(err.message || "Puting doc failed")
+    
+    
+    putEmployee = async (_id, data) => {
+        try {
+            console.log("dataSource putEmployee", _id, data)
+            const updatedEmployee = await Employee.findOneAndReplace(
+                {_id},
+                data,
+                {
+                    new: true,
+                    upsert: false,
+                }
+            )
+    
+            if(!updatedEmployee) throw new Error("No document found")
+    
+            return updatedEmployee
+    
+        } catch(err) {
+            console.log(err)
+            throw new Error(err.message || "Puting doc failed")
+        }
+    
     }
-
-}
-
-
-module.exports.deleteEmployee = async(_id) => {
-    try {
-        const deletedEmployee = await Employee.findByIdAndDelete(_id)
-        
-        if(!deletedEmployee) throw new Error("No document found")
-
-        return deletedEmployee
-    } catch(err) {
-        console.log(err)
-        throw new Error(err.message || "Deleting doc failed")
+    
+    
+    deleteEmployee = async(_id) => {
+        try {
+            const deletedEmployee = await Employee.findByIdAndDelete(_id)
+            
+            if(!deletedEmployee) throw new Error("No document found")
+    
+            return deletedEmployee
+        } catch(err) {
+            console.log(err)
+            throw new Error(err.message || "Deleting doc failed")
+        }
     }
 }
+
+
+module.exports = EmployeeDataSource
--- graphql/dataSources/upload.js
+++ graphql/dataSources/upload.js
@@ -1,27 +1,32 @@
 const cloudinary = require("../../utilities/cloudinary")
 
-module.exports.upload = async (
-    fileStream,
-) => {
-    try {
-        // console.log("fileStream", fileStream)
-
-        const { secure_url } = await new Promise((resolve, reject) => {
-            fileStream.pipe(
-                cloudinary.uploader.upload_stream((error, result) => {
-                    if(error) reject(error)
-
-                    resolve(result)
-                })
-            )
-        })
-        console.log("secure_url", secure_url)
-
-        return {
-            url: secure_url
+class UploadDataSource {
+    upload = async (
+        fileStream,
+    ) => {
+        try {
+            // console.log("fileStream", fileStream)
+    
+            const { secure_url } = await new Promise((resolve, reject) => {
+                fileStream.pipe(
+                    cloudinary.uploader.upload_stream((error, result) => {
+                        if(error) reject(error)
+    
+                        resolve(result)
+                    })
+                )
+            })
+            console.log("secure_url", secure_url)
+    
+            return {
+                url: secure_url
+            }
+        } catch(err) {
+            console.log(err)
+            throw new Error(err.message || "Uploading file failed")
         }
-    } catch(err) {
-        console.log(err)
-        throw new Error(err.message || "Uploading file failed")
-    }
-} 
+    } 
+}
+
+
+module.exports = UploadDataSource
